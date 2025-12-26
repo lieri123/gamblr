eef7191fbad2abf21cfc2f9aaeaf5bb156113cd3
lieri123 <lieric154@gmail.com>
2025-12-26 14:20:38 -0500

    Fetch user profile

---
+++ lib/actions/profile.ts
@@ -0,0 +1,29 @@
+"user server";
+
+import {createClient} from "../supabase/server";
+
+export async function getCurrentUserProfile(){
+    const supabase = await createClient()
+
+    const{
+        data:{ user },
+    } = await supabase.auth.getUser();
+
+    if (!user){
+        return null
+    }
+
+    const {data: profile, error} = await supabase
+        .from("users")
+        .select("*")
+        .eq("id", user.id)
+        .single();
+
+    if(error){
+        console.error("Error getting user profile: ", error);
+        return null;
+    }
+
+    return profile;
+
+}
